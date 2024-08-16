9aa08bab35eb08eeccfdaee49dda5ddb0656cd89
mn297 <peterwayne3293@gmail.com>
2024-08-15 23:58:47 -0400

    fixup! first commit

--- ros2_basics_ws/src/my_two_wheel_robot/launch/gazebo_robot_with_control.launch.py
+++ ros2_basics_ws/src/my_two_wheel_robot/launch/gazebo_robot_with_control.launch.py
@@ -1,39 +1,74 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, ExecuteProcess
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
 
-def generate_launch_description():
-    urdf_file = os.path.join(
-        # os.getenv('HOME'), 'ros2_ws/src/my_two_wheel_robot/urdf/two_wheel_robot.xacro')
-        get_package_share_directory('my_two_wheel_robot'), 'urdf/two_wheel_robot.xacro')
 
-    return LaunchDescription([
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([os.path.join(
-                get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
-        ),
-        Node(
-            package='gazebo_ros',
-            executable='spawn_entity.py',
-            arguments=['-entity', 'two_wheel_robot', '-file', urdf_file],
-            output='screen'
-        ),
-        Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            output='screen',
-            parameters=[{'robot_description': open(urdf_file).read()}]
-        ),
-        Node(
-            package='rviz2',
-            executable='rviz2',
-            name='rviz2',
-            output='screen',
-            arguments=['-d', os.path.join(
-                # os.getenv('HOME'), 'ros2_ws/src/my_two_wheel_robot/rviz/robot.rviz')]
-                get_package_share_directory('my_two_wheel_robot'), 'rviz/robot.rviz')]
-        )
-    ])
+def generate_launch_description():
+    # Get the path to the Xacro file
+    xacro_file = os.path.join(
+        get_package_share_directory("my_two_wheel_robot"),
+        "model",
+        "two_wheel_robot.xacro",
+    )
+
+    # Convert the Xacro file to a URDF file
+    urdf_file = "/tmp/two_wheel_robot.urdf"
+    urdf_file = os.path.join(
+        get_package_share_directory("my_two_wheel_robot"),
+        "model",
+        "two_wheel_robot.urdf",
+    )
+
+    # Xacro command to generate URDF
+    xacro_command = ["ros2", "run", "xacro", "xacro", xacro_file, "-o", urdf_file]
+
+    return LaunchDescription(
+        [
+            # # Convert Xacro to URDF
+            # ExecuteProcess(cmd=xacro_command, output="screen"),
+            # # Include the Gazebo launch file
+            # IncludeLaunchDescription(
+            #     PythonLaunchDescriptionSource(
+            #         [
+            #             os.path.join(
+            #                 get_package_share_directory("gazebo_ros"),
+            #                 "launch",
+            #                 "gazebo.launch.py",
+            #             )
+            #         ]
+            #     ),
+            # ),
+            # Spawn the entity in Gazebo
+            Node(
+                package="gazebo_ros",
+                executable="spawn_entity.py",
+                arguments=["-entity", "two_wheel_robot", "-file", urdf_file],
+                output="screen",
+            ),
+            # Publish robot state
+            Node(
+                package="robot_state_publisher",
+                executable="robot_state_publisher",
+                output="screen",
+                parameters=[{"robot_description": open(urdf_file).read()}],
+            ),
+            # Start RViz
+            Node(
+                package="rviz2",
+                executable="rviz2",
+                name="rviz2",
+                output="screen",
+                arguments=[
+                    "-d",
+                    os.path.join(
+                        get_package_share_directory("my_two_wheel_robot"),
+                        "rviz",
+                        "robot.rviz",
+                    ),
+                ],
+            ),
+        ]
+    )
--- ros2_basics_ws/src/my_two_wheel_robot/launch/turtlesim_mimic_launch.py
+++ ros2_basics_ws/src/my_two_wheel_robot/launch/turtlesim_mimic_launch.py
@@ -1,27 +1,23 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-def generate_launch_description():
-   return LaunchDescription([
 
-   ])
+
+def generate_launch_description():
+    return LaunchDescription([])
+
+
 Node(
-    package='turtlesim',
-    namespace='turtlesim1',
-    executable='turtlesim_node',
-    name='sim'
+    package="turtlesim", namespace="turtlesim1", executable="turtlesim_node", name="sim"
 ),
 Node(
-    package='turtlesim',
-    namespace='turtlesim2',
-    executable='turtlesim_node',
-    name='sim'
+    package="turtlesim", namespace="turtlesim2", executable="turtlesim_node", name="sim"
 ),
 Node(
-    package='turtlesim',
-    executable='mimic',
-    name='mimic',
+    package="turtlesim",
+    executable="mimic",
+    name="mimic",
     remappings=[
-      ('/input/pose', '/turtlesim1/turtle1/pose'),
-      ('/output/cmd_vel', '/turtlesim2/turtle1/cmd_vel'),
-    ]
+        ("/input/pose", "/turtlesim1/turtle1/pose"),
+        ("/output/cmd_vel", "/turtlesim2/turtle1/cmd_vel"),
+    ],
 )
